39e403907bbbb193f909c4a1d54fbc9cac66251e
amelhasic23 <117470943+amelhasic23@users.noreply.github.com>
2024-11-19 10:30:59 +0100

    Commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exercises
+{
+    class ConstantRenaming
+    {
+        static void Main(string[] args)
+        {
+            const short PAGE_WIDTH = 800;
+
+            Console.WriteLine($"Page width: {PAGE_WIDTH}");
+        }
+    }
+}
+
